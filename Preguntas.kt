7877bf1c4b81a85ae2da64e58b68c9ac286cd8c2
AlexJJG <49285979+AlexJJG@users.noreply.github.com>
2019-05-01 21:29:55 -0600

    Preguntas
    Resueltas

---
+++ Preguntas.kt
@@ -0,0 +1,32 @@
+¿Qué es control de versiones?
+Se llama control de versiones a la gestión de los diversos cambios que se realizan sobre los elementos de algún producto o una configuración del mismo. Una versión, revisión o edición de un producto, es el estado en el que se encuentra el mismo en un momento dado de su desarrollo o modificación.
+
+¿Cuáles son las ventajas de utilizar un controlador de versiones?
+- Muy versátil y con figurable.
+- Distribuido, podemos hace commits en nuestra maquina local, por lo que podemos seguir trabajando sin conexión a Internet subir los cambios mas tarde.
+- Rápido, la mayoría de operaciones son locales, resolución de conflictos a travez de herramientas  mergetool y el editor meld.
+- Seguro, se puede trabajar incluso si el servidor esta caído sin perder tiempo de trabajo.
+- La posibilidad de ver  rápidamente la historia del proyecto con el comando git log.
+
+¿Cuáles son los controladores de versiones disponibles para android studio?
+- Subversion
+- CVS
+- Git
+- ClearCase
+- Google Cloud
+
+¿Cuáles son los servidores de controladores de versiones más conocidos?
+- Git
+
+Investigue los siguientes términos:
+
+Push: La traducción literal sería: rama. Es decir, dentro de nuestro sistema de control de versiones podemos ver el histórico de
+cambios como si de un árbol se tratase. 
+
+Commit: La operación commit indica la finalización de una transacción satisfactoria. Indica al administrador de transacciones que una unidad de trabajo lógica ha concluido satisfactoriamente, que la base de datos está o debería estar nuevamente en un estado consistente.
+
+DELETE: La instrucción DELETE permite eliminar uno o múltiples registros. Incluso todos los registros de una tabla, dejándola vacía.
+
+Stack: Una pila (stack en inglés) es una lista ordenada o estructura de datos que permite almacenar y recuperar datos, el modo de acceso a sus elementos es de tipo LIFO (del inglés Last In, First Out, «último en entrar, primero en salir») 
+
+Repositorio: Repositorio es un término que tiene su raíz etimológica en repositorium, un vocablo latino. Un repositorio es un espacio que se utiliza para almacenar distintas cosas. 
